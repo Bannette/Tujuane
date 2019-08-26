b806473411336582c72edfa46434d1f88578be5e
Bannette <banetaloo3@gmail.com>
2019-08-26 10:24:01 +0300

    added an array adapter

--- app/src/main/java/com/moringaschool/tujuane/MyFriendsArrayAdapter.java
+++ app/src/main/java/com/moringaschool/tujuane/MyFriendsArrayAdapter.java
@@ -1,4 +1,29 @@
 package com.moringaschool.tujuane;
 
-public class MyFriendsArrayAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class MyFriendsArrayAdapter extends ArrayAdapter {
+    private Context mContext;
+    private String[] mFriends;
+    private String[] mDescription;
+
+    public MyFriendsArrayAdapter(Context mContext, int resource, String[] mFriends, String[] mDescription){
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mDescription = mDescription;
+        this.mFriends = mFriends;
+    }
+
+    @Override
+    public Object getItem(int position){
+        String restaurant = mFriends[position];
+        String cuisine = mDescription[position];
+        return String.format("%s \n Serves great: %s", restaurant, cuisine);
+    }
+
+    @Override
+    public int getCount(){
+        return mDescription.length;
+    }
 }
