b96cc20a50c95831fd7954d2b3c8f38c62f6a503
Bannette <banetaloo3@gmail.com>
2019-09-04 11:28:28 +0300

    created constant java

--- app/src/main/java/com/moringaschool/tujuane/Constants.java
+++ app/src/main/java/com/moringaschool/tujuane/Constants.java
@@ -1,4 +1,7 @@
 package com.moringaschool.tujuane;
 
 public class Constants {
+    public static final String YELP_TOKEN = BuildConfig.MEET_TOKEN;
+    public static final String YELP_BASE_URL = "https://secure.meetup.com/meetup_api/oauth_consumers/";
+    public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
 }
